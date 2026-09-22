7b330a4e078e7b6a9248f1e93818ba27de671ad1
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-09-22 08:48:56 -0400

    revert commit

--- frontend/app.js
+++ frontend/app.js
@@ -32,7 +32,7 @@ async function refreshCurrentProfilePhoto() {
 
   try {
     const token = getToken();
-    const response = await fetchProtegido(
+    const response = await fetch(
       `${API_URL}/usuarios/${state.user.id}/foto?t=${Date.now()}`,
       {
         headers: token ? { Authorization: `Bearer ${token}` } : {}
